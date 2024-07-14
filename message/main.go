401f3b21d535ce5bf9090e71649e64ca33f2ac14
John van Zantvoort <john@vanzantvoort.org>
2024-07-14 17:20:53 +0200

    minor add messages struct

--- message/main.go
+++ message/main.go
@@ -1,5 +1,10 @@
 package message
 
+import (
+	"encoding/json"
+	"io"
+)
+
 //
 //
 
@@ -10,4 +15,20 @@ type Message struct {
 	Priority int    `json:"priority"` // priority of the message
 	Activity int64  `json:"active"`   // int time (unix time) of last time activated
 	Created  int64  `json:"created"`  // int time (unix time) of creation time
+}
+
+type Messages []Message
+
+func (M Messages) Read(handle io.Reader) error {
+	return json.NewDecoder(handle).Decode(&M)
+}
+
+func (M Messages) Write(handle io.Writer) error {
+	json.Marshal(M)
+	return nil
+}
+
+// NewMessage returns a new message.
+func NewMessage() *Message {
+	return &Message{}
 }
